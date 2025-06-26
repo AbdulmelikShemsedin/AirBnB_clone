45139a138b27ec6a239721ec7261e1fdc7f9dc48
nati-sahlu <natansahlu5@gmail.com>
2025-06-26 13:47:59 +0300

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -27,14 +27,14 @@ class BaseModel:
 
     def save(self):
         """
-
+        updates the public instance attribute updated_at with the current datetime
         """
         self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
         """
-
+        returns a dictionary containing all keys/values of __dict__ of the instance
         """
         inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
@@ -44,33 +44,6 @@ class BaseModel:
         return inst_dict
 
     def __str__(self):
-        """
+        """Returns string: [<class name>] (<self.id>) <self.__dict__>"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-        """
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
-
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My_First_Model"
-    my_model.my_number = 89
-    print(my_model.id)
-    print(my_model)
-    print(type(my_model.created_at))
-    print("--")
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - \
-        {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-    print(my_model is my_new_model)
